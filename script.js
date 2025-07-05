850d894f38061ea067d8301b858d89829cc6667d
lulu472 <lucas.curis08@gmail.com>
2025-07-05 12:06:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "TA_API_KEY",
+  authDomain: "agenda-evenements-xxxx.firebaseapp.com",
+  databaseURL: "https://agenda-evenements-xxxx-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "agenda-evenements-xxxx",
+  storageBucket: "agenda-evenements-xxxx.appspot.com",
+  messagingSenderId: "xxxxxxx",
+  appId: "xxxxxxx"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
 const form = document.getElementById('event-form');
 const eventList = document.getElementById('event-list');
 
